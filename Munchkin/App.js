259b87cc4b2118084f6d717173a265082c4eeec8
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-10-16 10:13:45 +0200

    elso_ora

--- Munchkin/App.js
+++ Munchkin/App.js
@@ -1,31 +1,129 @@
-import React, {Component} from 'react';
-import {Alert, Button, StyleSheet, View} from 'react-native';
+import React, {Component, useState, useEffect} from 'react';
+import {Alert, Button, StyleSheet, View, Text} from 'react-native';
 
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!');
+export default function App() {
+  const [level, setLevel] = useState(1)
+  const [score, setScore] = useState(1)
+  const [dice, setDice] = useState(6)
+
+  function levelUp()
+  {
+    if (level < 10)
+    {
+      setLevel(level + 1)
+    }
+  }
+  function levelDown()
+  {
+    if (level > 1)
+    {
+      setLevel(level - 1)
+    }
   }
 
-  render() {
+  function scoreUp()
+  {
+    if (score < 10)
+    {
+      setScore(score + 1)
+    }
+  }
+  function scoreDown()
+  {
+    if (score > 1)
+    {
+      setScore(score - 1)
+    }
+  }
+
+  function diceUp()
+  {
+    if (dice < 20)
+    {
+      setDice(dice + 1)
+    }
+  }
+  function diceDown()
+  {
+    if (dice > 1)
+    {
+      setDice(dice - 1)
+    }
+  }
+
+
     return (
       <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button onPress={this._onPressButton} title="Press Me" />
+
+        <View style={[
+                styles.container,
+                styles.upperbox,
+                {
+                  // Try setting `flexDirection` to `"row"`.
+                  flexDirection: 'row',
+                  flex: 1,
+                },
+              ]}>
+          <View style={[
+            styles.boxes,
+            {
+              flex: 1
+            }
+            ]}>
+            <Text style={styles.titles}>SZINTEK száma:</Text>
+            <View style={styles.gomb}>
+              <Button onPress={levelUp} title="+" color="#841584"/>
+            </View>
+            <Text style={styles.szoveg}>{level}</Text>
+            <View style={styles.gomb}>
+              <Button onPress={levelDown} title="-" color="#841584"/>
+            </View>
+          </View>
+
+          <View style={[
+            styles.boxes,
+            {
+              flex: 1
+            }
+            ]}>
+            <Text style={styles.titles}>PONTOK száma:</Text>
+            <View style={styles.gomb}>
+              <Button onPress={scoreUp} title="+" color="#841584"/>
+            </View>
+            <Text style={styles.szoveg}>{score}</Text>
+            <View style={styles.gomb}>
+              <Button onPress={scoreDown} title="-" color="#841584"/>
+            </View>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button onPress={this._onPressButton} title="This looks great!" />
-          <Button onPress={this._onPressButton} title="OK!" color="#841584"/>
+
+
+
+        <View style={[
+                styles.container,
+                styles.lowerbox,
+                {
+                  // Try setting `flexDirection` to `"row"`.
+                  flexDirection: 'row',
+                },
+              ]}>
+          <View style={[
+            {
+              flex: 1
+            }
+            ]}>
+            <Text style={styles.dice_title}>Kocka oldalszáma:</Text>
+            <View style={styles.gomb}>
+              <Button onPress={diceUp} title="+" color="#841584"/>
+            </View>
+            <Text style={styles.szoveg}>{dice}</Text>
+            <View style={styles.gomb}>
+              <Button onPress={diceDown} title="-" color="#841584"/>
+            </View>
+          </View>
         </View>
       </View>
     );
-  }
 }
 
 const styles = StyleSheet.create({
@@ -33,12 +131,47 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
-  buttonContainer: {
-    margin: 20,
+  lowerbox:
+  {
+    borderTopColor: 'black',
+    borderTopWidth: 1,
+    paddingTop: 70,
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  gomb: {
+    marginVertical: 10,
+    marginHorizontal: 'auto',
+    width: 100,
+  },
+  szoveg:
+  {
+    textAlign: 'center',
+    backgroundColor: 'purple',
+    marginHorizontal: 50,
+    color: 'white',
+    fontSize: 20,
+    padding: 5,
+  },
+  titles:
+  {
+    textAlign: 'center',
+    fontSize: 25,
+    backgroundColor: 'purple',
+    color: 'white',
+    padding: 10,
+  },
+  boxes:
+  {
+    marginVertical: 'auto',
+    margin: 10,
+    backgroundColor: 'pink',
+    height: 'auto',
+  },
+  dice_title:
+  {
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: 'purple',
+    marginHorizontal: 10,
+    fontSize: 25,
   },
 });
